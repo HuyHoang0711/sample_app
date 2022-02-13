411f35bc8e28f2a50e0eeca42e52ab6ff23bbeed
HuyHoang0711 <huyhuyhoangb45@gmail.com>
2022-02-13 11:27:04 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params.dig(:session, :email)&.downcase
     if user&.authenticate(params.dig(:session, :password))
       log_in user
+      remember_or_forget user
       redirect_to user
     else
       flash.now[:danger] = t("log_in_failed")
@@ -12,9 +13,12 @@ class SessionsController < ApplicationController
     end
   end
 
-
   def destroy
     log_out
     redirect_to root_url
+  end
+
+  def remember_or_forget user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
+  attr_accessor :remember_token
+
   validates :name, presence: true, length: {maximum: Settings.digits.digit_50}
 
   validates :email, presence: true,
@@ -22,6 +24,23 @@ class User < ApplicationRecord
              end
       BCrypt::Password.create(string, cost: cost)
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 
   private
